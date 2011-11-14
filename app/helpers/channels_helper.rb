7b699d3c78934c3a5f5d490ead103a393055ce4f
Jeffrey Noh <jnoh12388@gmail.com>
2011-11-14 02:43:00 -0800

    updated the order in zee helper

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,7 +1,7 @@
 module ChannelsHelper
   
   def top_channel
-    @top ||= Channel.order("subscriptions_count DESC").first
+    @top ||= Channel.order("subscriptions_count DESC, updated_at DESC").first
   end
   
   def is_top?(channel)
