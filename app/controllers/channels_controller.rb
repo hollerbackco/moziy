6359a9574525f3f6cf161f3773a1cc015e2be2d9
Jeffrey Noh <jnoh12388@gmail.com>
2011-11-14 00:07:32 -0800

    fix order

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,7 +2,7 @@ class ChannelsController < ApplicationController
   before_filter :require_login, :except => [:show]
   
   def index
-    @channels = Channel.all(:order => "subscriptions_count DESC")
+    @channels = Channel.find(:all, :order => "subscriptions_count DESC, updated_at DESC")
   end
   
   def new
