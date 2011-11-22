5ef6398636d5c288481ea41249cde11f1d6a8a15
Jeffrey Noh <jnoh12388@gmail.com>
2011-11-21 21:40:27 -0800

    Compiled assets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   def home
     redirect_to Channel.order("subscriptions_count DESC, updated_at DESC").first
   end
+  
+  def about
+    
+  end
 
 end
