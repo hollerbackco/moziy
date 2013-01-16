0baa29b25fdd0530d61d49c1b6e29f96b270133e
Jeffrey Noh <jnoh12388@gmail.com>
2013-01-16 04:45:37 -0800

    inbox

--- app/models/airing.rb
+++ app/models/airing.rb
@@ -1,6 +1,6 @@
 class Airing < ActiveRecord::Base
   acts_as_nested_set
-  acts_as_readable
+  acts_as_readable on: :created_at
 
   attr_accessible :video, :channel, :parent, :video_id, :channel_id, :parent_id, :state
 
