af941cb594d0e029e59c71c57804cd124ed320b2
Jeffrey Noh <jnoh12388@gmail.com>
2013-01-17 17:03:28 -0800

    player css

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,6 @@ module Puretv
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.paths << Rails.root.join("application", "player")
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
