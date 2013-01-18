8b37c7ca406a88399b7fef81e0aea2015a6113fc
Jeffrey Noh <jnoh12388@gmail.com>
2013-01-17 22:08:35 -0800

    fix likes controler

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     airing = channel.airings.find params[:airing_id]
 
     if airing.liked_by current_user
-      ChannelMailer.liked(current_user, channel, video.title).deliver
+      ChannelMailer.liked(current_user, channel, airing.title).deliver
       render nothing: true, status: :ok
     else
       render nothing: true, status: 400
