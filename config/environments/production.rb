57c7fd55b1797666421bad46b1963ed47b22e39a
Jeffrey Noh <jnoh12388@gmail.com>
2013-01-22 00:23:26 -0800

    domains added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,6 @@ Puretv::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  config.action_mailer.default_url_options = { :host => 'outlaw.mosey.tv' }
+
+  config.action_mailer.default_url_options = { :host => 'new.mosiy.com' }
 end
