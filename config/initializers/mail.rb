56832edf5d50e528655967656f6621438a9f6c4b
Jeffrey Noh <jnoh12388@gmail.com>
2013-02-05 16:30:22 -0800

    fix sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -10,6 +10,15 @@ if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
 
 elsif Rails.env.staging?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.register_interceptor(PreventMailInterceptor)
 
 elsif Rails.env.development?
