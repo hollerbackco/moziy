99e453cf03063fde65e1940279b420e8c09654c8
Jeffrey Noh <jnoh12388@gmail.com>
2013-03-06 02:39:06 -0500

    fix sorcery

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -163,16 +163,16 @@ Rails.application.config.sorcery.configure do |config|
 
     user.reset_password_mailer = UserMailer                                                     # mailer class. Needed.
 
-    user.reset_password_email_method_name = :reset_password_email                             # reset password email
+    #user.reset_password_email_method_name = :reset_password_email                             # reset password email
                                                                                                 # method on your mailer
                                                                                                 # class.
 
-    # user.reset_password_expiration_period = nil                                               # how many seconds
+    user.reset_password_expiration_period = 10.minutes                                               # how many seconds
                                                                                                 # before the reset
                                                                                                 # request expires. nil
                                                                                                 # for never expires.
 
-    user.reset_password_time_between_emails = nil                                          # hammering protection,
+    user.reset_password_time_between_emails = 5 * 60                                          # hammering protection,
                                                                                                 # how long to wait
                                                                                                 # before allowing
                                                                                                 # another email to be
