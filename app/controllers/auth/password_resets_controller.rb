804e62e473adf781d0e77540437feaf00a9fc27e
Jeffrey Noh <jnoh12388@gmail.com>
2013-03-06 01:47:55 -0500

    password resets fix

--- app/controllers/auth/password_resets_controller.rb
+++ app/controllers/auth/password_resets_controller.rb
@@ -7,13 +7,9 @@ class Auth::PasswordResetsController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
-    if @user
-      @user.deliver_reset_password_instructions!
+    if @user and @user.deliver_reset_password_instructions!
       UserMailer.reset_password_email(@user).deliver
     end
-
-    # Tell the user instructions have been sent whether or not email was found.
-    # This is to not leak information to attackers about which emails exist in the system.
     redirect_to(login_path, :notice => 'Instructions have been sent to your email.')
   end
 
