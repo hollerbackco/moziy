579ee2188d8749d619542450599df093412f232a
Jeffrey Noh <jnoh12388@gmail.com>
2013-03-06 02:46:29 -0500

    email once on password resetg

--- app/controllers/auth/password_resets_controller.rb
+++ app/controllers/auth/password_resets_controller.rb
@@ -8,7 +8,6 @@ class Auth::PasswordResetsController < ApplicationController
 
     # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
     if @user and @user.deliver_reset_password_instructions!
-      UserMailer.reset_password_email(@user).deliver
       redirect_to(login_path, :notice => 'Instructions have been sent to your email.')
     else
       redirect_to(login_path, :notice => 'Instructions have already been sent.')
