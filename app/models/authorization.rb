57c78f91e2f9e17b2d741dba43565e7f08dc9c6c
Jeffrey Noh <jnoh12388@gmail.com>
2013-04-03 04:47:59 -0700

    fix expires logic

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -5,6 +5,6 @@ class Authorization < ActiveRecord::Base
   belongs_to :user
 
   def expired?
-    expires_at.present? ? (DateTime.now > self.expires_at) : false
+    expires_at.present? ? (DateTime.now > self.expires_at) : true
   end
 end
